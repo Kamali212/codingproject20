8002f08a9372eae84a70295837b95b37c1c2c694
Kamali Hicks <kamalikh3@gmail.com>
2025-04-18 19:44:48 -0400

    App Component Structure

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Gallery from './components/Gallery';
+import './styles/styles.css';
+//Variables for the App component
+function App() {
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+// Fetch tours from the API 
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch('https://course-api.com/react-tours-project');
+      const data = await res.json();
+      setTours(data);
+      setError(null);
+    } catch (err) {
+      setError(true);
+      setTours([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+// fetch tours when the component mounts
+  useEffect(() => {
+    fetchTours();
+  }
+  , []);
+// Function to remove a tour
+  const removeTour = (id) => {
+    const updatedTours = tours.filter(tour => tour.id !== id);
+    setTours(updatedTours);
+  };
+// Render App component
+  return (
+    <main>
+      <h1>Our Tours</h1>
+      <Gallery tours={tours} onRemoveTour={removeTour} loading={loading} error={error}/>
+      <button onClick={fetchTours}>Fetch new tours</button>
+    </main>
+  );
+}
+
+export default App;
